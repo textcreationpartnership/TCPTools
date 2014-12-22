1d6553f1b1ff78ddaf22a2c52b0aee22ede32d12
Sebastian Rahtz <sebastian.rahtz@gmail.com>
2014-12-22 11:17:43 +0000

    set up job to make GHub repos

--- create-repo.sh
+++ create-repo.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-name=$1
-user=$2
-password=$3
-test -z $name && echo "Repo name required." 1>&2 && exit 1
+user=$1
+password=$2
 scriptdir=`dirname "$0"`
 APPHOME=`(cd $scriptdir; pwd)`
-mkdir $name
-cd $name
-D=`echo $name | sed 's/\(...\).*/\1/'`
-cp /Users/rahtz/TEI/tei.oucs.ox.ac.uk/Projects/EEBO/Texts-TEI/free/$D/$name.xml .
-saxon $name.xml $APPHOME/MDown/genReadme.xsl >README.md
-curl -u $user:$password https://api.github.com/orgs/textcreationpartnership/repos -d "{\"name\":\"$name\"}"
-git init
-git add $name.xml README.md
-git commit -a -m "add text file and README"
-git remote add origin "https://github.com/textcreationpartnership/$name.git"
-git push origin master
+for i in `find /Users/rahtz/TEI/tei.oucs.ox.ac.uk/Texts-TEI/free`
+do
+ name=`filename $i`
+ mkdir $name
+ cd $name
+ cp $i .
+ saxon $name.xml $APPHOME/MDown/genReadme.xsl >README.md
+ curl -u $user:$password https://api.github.com/orgs/textcreationpartnership/repos -d "{\"name\":\"$name\"}"
+ git init
+ git add $name.xml README.md
+ git commit -a -m "add text file and README"
+ git remote add origin "https://github.com/textcreationpartnership/$name.git"
+ git push origin master
+ cd ..
+done
